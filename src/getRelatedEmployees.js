4a6a4fab6e0adcdb8fb4ca7e77350392cc09dfad
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-03 14:28:27 -0400

    Dev: Requisito 4.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const { employees } = data;
+
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
+// console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter(({ managers }) => managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+// console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
