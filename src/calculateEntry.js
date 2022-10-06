936f1514cbcd51b9b1464a63a678132fa3d533db
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-05 23:52:48 -0400

    Dev: Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+// const object = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
-
+const countEntrants = (entrants) => ({
+  child: entrants.filter(({ age }) => age < 18).length,
+  adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+  senior: entrants.filter(({ age }) => age >= 50).length,
+});
+// { child: 3, adult: 2, senior: 1 }
+const calculateEntry = (entrants = 0) => {
+  if (entrants === undefined || Object.entries(entrants).length === 0) { return 0; }
+  const visitantes = countEntrants(entrants);
+  return visitantes.child * prices.child
+  + visitantes.adult * prices.adult
+  + visitantes.senior * prices.senior;
+};
+// console.log(calculateEntry(object));
 module.exports = { calculateEntry, countEntrants };
