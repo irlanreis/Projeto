a31e087f5221dcb0757fd6773adde22639ec0ab2
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-08 18:03:48 -0400

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(ids) {
+  const getPeople = employees.filter(({ id }) => id === ids)[0].responsibleFor;
+  const theAnimal = species.find(({ id }) => id === getPeople[0]).residents
+    .sort((a, b) => b.age - a.age)[0];
+  return Object.values(theAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
